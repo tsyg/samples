12e47c65898ad66693696a3e9a52f55c2aab7ec3
Yuri Tsyganenko <Yuri.Tsyganenko@dataart.com>
2023-09-05 18:32:50 +0400

    Samples of async usage

---
+++ play_async.py
@@ -0,0 +1,34 @@
+#  Playing  with async
+# see https://docs.python.org/3/library/asyncio-task.html
+import asyncio
+import time
+
+
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+# async def main():
+#     print('hello')
+#     await asyncio.sleep(1)
+#     print('world')
+
+
+async def main():
+    task1 = asyncio.create_task(
+        say_after(1, 'hello'))
+
+    task2 = asyncio.create_task(
+        say_after(2, 'world'))
+
+    print(f"started at {time.strftime('%X')}")
+
+    # Wait until both tasks are completed (should take
+    # around 2 seconds.)
+    await task1
+    await task2
+
+    print(f"finished at {time.strftime('%X')}")
+
+
+asyncio.run(main())
